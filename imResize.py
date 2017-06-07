7dd015fb6fa637cd4bdc76917dcd727d225dbbb4
alexQQ <alex@quadquestions.com>
2017-06-07 17:05:15 -0600

    Updated imResize to accept arguments

--- imResize.py
+++ imResize.py
@@ -3,12 +3,25 @@
 #############################################
 import os
 import cv2
-rootdir = "Example/Directory"
+import argparse
 
-for subdir, dirs, files in os.walk(rootdir):
+parser = argparse.ArgumentParser(description='Resize images in a given directory to given size.')
+
+parser.add_argument("directory",
+			help="Directory address of images to resize")
+
+parser.add_argument('--height', type=int, default=50,
+            help="New height of the image in pixels")
+
+parser.add_argument('--width', type=int, default=50,
+            help="New width of the image in pixels")
+
+args = parser.parse_args()
+
+for subdir, dirs, files in os.walk(args.directory):
     for file in files:
         im = os.path.join(subdir, file)
         print im
         image = cv2.imread(im)
-        out = cv2.resize(image, (50, 50))
+        out = cv2.resize(image, (args.width, args.height))
         cv2.imwrite(im, out)

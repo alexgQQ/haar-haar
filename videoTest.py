89deaf9e71ca1dce9e1c96ee09167583d47c4a0e
alexQQ <alex@quadquestions.com>
2017-06-07 17:25:34 -0600

    Updated to accept command line args for xml file. Added info on function variable for tuning.

--- videoTest.py
+++ videoTest.py
@@ -1,22 +1,41 @@
-#############################################
-#	Code to test detection with a USB camera
-#############################################
+################################################################################################
+#
+#	Video Detection Test
+#
+###############################################################################################
+#
+#	This will detect the object trained for from a USB video feed.
+#	Usefull to see the detection in real time. 
+#	Press ESC to close the camera window when using.
+#
+###############################################################################################
+
 import numpy as np
 import cv2
+import argparse
 
-# This is the cascade xml file made in training.
-cascade = cv2.CascadeClassifier('Cascades\cascade.xml')
+parser = argparse.ArgumentParser(description='Will use a XML cascade file generated from OpenCV Haar training to detect objects. Supply a target directory of images to test. A box will be drawn around detection points. The images in the directory will be overwritten.')
 
-cap = cv2.VideoCapture(0)
+parser.add_argument("xml",
+			help="Location of cascade xml file")
 
-lower = np.array([1, 1, 100])  #BGR
-upper = np.array([70, 60, 200])
-facial = False
-color = False
+args = parser.parse_args()
 
-n = 10
-scaleF = 1.2
-minSize = (50, 50)
+cascade = cv2.CascadeClassifier(args.xml)
+
+cap = cv2.VideoCapture(0)		#	Camera ID, defaults to the first camera detected
+
+#############################################
+#	Tunable variables to check for
+#############################################
+
+n = 10					#	Number of neighbors that the detector will validate against. 
+						#	The higher the numnber, the more noise is filtered but may miss a target.
+
+scaleF = 1.2			#	Scale factor of the trained object to detect for
+
+minSize = (50, 50)		#	Minimum size of the target object trained for. 
+						# 	Should match the size that the image was trained on.
 
 while 1:
     ret, img = cap.read()
@@ -25,7 +44,7 @@ while 1:
         cv2.rectangle(img,(x,y),(x+w,y+h),(127,255,0),2)
     
     cv2.imshow('OUT',img)
-    k = cv2.waitKey(30) & 0xff
+    k = cv2.waitKey(30) & 0xff		#	Press ESC to close the camera window when using.
     if k == 27:
         break
 
